24427c83469f02fdf7c2bd7b83ca4c112e3abb1f
severtut <bogatkina.olga@gmail.com>
2023-11-21 13:56:50 +0300

    Lesson2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        printThreeWords();
+        checkSumSign(1, 2);
+        printColor(101);
+        int a = 10;
+        int b = 5;
+        compareNumbers(a, b);
+    }
+    static void printThreeWords() {
+        System.out.println("Orange ");
+        System.out.println("Banana ");
+        System.out.println("Apple ");
+    }
+
+    static void checkSumSign(int x, int y) {
+        int z = x + y;
+        if (z >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    static void printColor(int z) {
+        if (z <= 0) {
+            System.out.println("Красный");
+        } else if (0 < z && z <= 100){
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    static void compareNumbers(int x, int y) {
+        if (x >= y) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }
